d4db4ceaf7cfcd6f59244bc92903d3aeb6937bf5
demathieu <mathieuthys@hotmail.com>
2015-11-30 22:22:52 +0100

    building problem example

--- Wrapping/alice.js
+++ Wrapping/alice.js
@@ -1,3 +1,5 @@
 "use strict";
 
-var alice = {firstname:'Jan',lastName:'Smith',listOfCoupons:[],friendList:[], stealCoupon : function(){}};
+var alice = {firstname:'Jan',lastName:'Smith',listOfCoupons:[],friendList:[], stealCoupon : function(index){return this.friendList[index].listOfCoupons[index];}};
+
+module.exports = alice;
---
+++ Wrapping/bob.js
@@ -0,0 +1,5 @@
+"use strict";
+
+var bob = {firstname:'Jan',lastName:'Smith',listOfCoupons:[{id : 1}],friendList:[], stealCoupon : function(){}};
+
+module.exports = bob;
