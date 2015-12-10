02695c11ad654f49c3023abc2bd617211ca36d1f
demathieu <mathieuthys@hotmail.com>
2015-12-10 13:57:22 +0100

    added whitelist to load function + some documents

---
+++ Wrapping/ModifiedInterface.js
@@ -0,0 +1,41 @@
+var harmony =require('harmony-reflect');
+
+
+function createHandler(whiteList){
+
+	return handler = {
+		get: function (target,name){
+			console.log("test");
+			console.log(whiteList);
+			if (whiteList.indexOf(whiteList) == -1){
+				console.log('No access');
+			}
+			else{
+				return target[name];
+			}	
+		}
+	}
+
+}
+
+
+
+function load (filePath,whiteList){
+	var importedObject = require (filePath);
+	var handler = createHandler(whiteList);
+	var proxyClient = new Proxy(importedObject,handler);
+	return proxyClient;
+}
+///////////////////////////////////////////////////////////////////// ALL Above is Backhand
+
+
+var whiteList = ['stealCoupon'];
+var alice = load('./alice.js',whiteList)
+var bob = load('./bob.js',whiteList)
+
+alice.friendList.push(bob);
+
+console.log(alice.stealCoupon(0));
+
+
+
--- Wrapping/bob.js
+++ Wrapping/bob.js
@@ -1,5 +1,15 @@
 "use strict";
 
-var bob = {firstname:'Jan',lastName:'Smith',listOfCoupons:[{id : 1}],friendList:[], stealCoupon : function(){}};
+var bob = {
+	firstname:'Jan',
+	lastName:'Smith',
+	listOfCoupons:[{id : 1}],
+	friendList:[], 
+	stealCoupon : function(){}};
+	spendCoupon : function(amount){
+		for (i = 0; i == amount; i++){
+			listOfCoupons.shift();
+		}
+	}
 
 module.exports = bob;
